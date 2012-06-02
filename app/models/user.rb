37e5b0b9177795407c183decf6b0034546b8c124
John Wallom <jwallom@gmail.com>
2012-06-01 23:21:10 -0400

    Starting to replace user scaffolding with code.  First steps to moving to an actual authentication system.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ActiveRecord::Base
+
+validates_presence_of :username, :message => "You must enter a username."
+validates_confirmation_of :password, :message => "Password cannot be blank"
+validates_presence_of :password, :on => :create
+
 end
