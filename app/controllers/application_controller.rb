51e5844e5140c11f42582dae1175489511b14e0f
John Wallom <jwallom@gmail.com>
2012-06-03 21:45:12 -0400

    Added basic logic for instructor only functions.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,22 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   helper_method :current_user
+  helper_method :instructor?
 
   private
 
   def current_user
   	@current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+  
+  private
+  
+  def instructor?
+  	user = User.find(session[:user_id]) if session[:user_id]
+  	unless user.instructor
+  		return false
+  	else
+  		return true
+  	end
   end
 end
